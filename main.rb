097963862306a6982843df83188c3a5866fbb6a2
skytomo <tomo1kawa.fullerene@gmail.com>
2018-11-08 23:48:31 +0900

    [Change] indent

--- main.rb
+++ main.rb
@@ -2,33 +2,33 @@ require 'discordrb'
 require 'timers'
 
 year_roles = [
-    "2014",
-    "2015",
-    "2016",
-    "2017",
-    "2018",
+  "2014",
+  "2015",
+  "2016",
+  "2017",
+  "2018",
 ]
 
 major_roles = [
-    "Mechanical",
-    "Electrical and Electronic",
-    "Electronic Control",
-    "Information",
-    "Department of Urban Environmental Design",
+  "Mechanical",
+  "Electrical and Electronic",
+  "Electronic Control",
+  "Information",
+  "Department of Urban Environmental Design",
 ]
 
 games = [
-    "Bad North",
-    "Dead by Daylight",
-    "Hearthstone",
-    "Minecraft",
-    "Moonlighter",
-    "Overwatch",
-    "PLYAERUNKNOWN'S BATTLEGROUNDS",
-    "StarCraft II",
-    "Yandere Simulator",
-    "黒い砂漠",
-    "見上げてごらん夜の星を",
+  "Bad North",
+  "Dead by Daylight",
+  "Hearthstone",
+  "Minecraft",
+  "Moonlighter",
+  "Overwatch",
+  "PLYAERUNKNOWN'S BATTLEGROUNDS",
+  "StarCraft II",
+  "Yandere Simulator",
+  "黒い砂漠",
+  "見上げてごらん夜の星を",
 ]
 
 token = File.read('token.dat')
@@ -42,141 +42,141 @@ client_id.chomp!
 bot = Discordrb::Commands::CommandBot.new token: token, client_id: client_id, prefix: prefix
 
 bot.ready do |event|
-    bot.game = bot.game = games[rand(games.count)]
+  bot.game = bot.game = games[rand(games.count)]
 end
 
 bot.member_join do |event|
-    event.server.text_channels[0].send_embed do |embed|
-        embed.title = event.user.name + "さんが参加しました"
-        embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(
-            url: event.user.avatar_url
-        )
-        embed.color = '00dd00'
-        embed.add_field(
-            name: 'シス研へようこそ！',
-            value: "このサーバーにはいくつかのルールがあります。\nはじめに #readme をお読みください。",
-        )
-    end
+  event.server.text_channels[0].send_embed do |embed|
+    embed.title = event.user.name + "さんが参加しました"
+    embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(
+      url: event.user.avatar_url
+    )
+    embed.color = '00dd00'
+    embed.add_field(
+      name: 'シス研へようこそ！',
+      value: "このサーバーにはいくつかのルールがあります。\nはじめに #readme をお読みください。",
+    )
+  end
 end
 
 bot.command :test do |event|
-    event.server.text_channels[0].send_embed do |embed|
-        embed.title = event.user.name + "さんが参加しました"
-        embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(
-            url: event.user.avatar_url
-        )
-        embed.color = '00dd00'
-        embed.add_field(
-            name: 'シス研へようこそ！',
-            value: "このサーバーにはいくつかのルールがあります。\nはじめに #readme をお読みください。",
-        )
-    end
+  event.server.text_channels[0].send_embed do |embed|
+    embed.title = event.user.name + "さんが参加しました"
+    embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(
+      url: event.user.avatar_url
+    )
+    embed.color = '00dd00'
+    embed.add_field(
+      name: 'シス研へようこそ！',
+      value: "このサーバーにはいくつかのルールがあります。\nはじめに #readme をお読みください。",
+    )
+  end
 end
 
 # /neko: にゃーんと鳴きます
 bot.command :neko do |event, *args|
-    event << "#{args.join(' ')}にゃーん"
+  event << "#{args.join(' ')}にゃーん"
 end
 
 # /random [min] [max]: min以上max以下の乱数を返します
 # /random [max]: max未満の乱数を返します
 bot.command(:random, min_args: 0, max_args: 2, description: 'Generates a random number between 0 and 1, 0 and max or min and max.', usage: 'random [min/max] [max]') do |event, min, max|
-    # The `if` statement returns one of multiple different things based on the condition. Its return value
-    # is then returned from the block and sent to the channel
-    if max
-        rand(min.to_i..max.to_i)
-    elsif min
-        rand(0..min.to_i)
-    else
-        rand
-    end
+  # The `if` statement returns one of multiple different things based on the condition. Its return value
+  # is then returned from the block and sent to the channel
+  if max
+    rand(min.to_i..max.to_i)
+  elsif min
+    rand(0..min.to_i)
+  else
+    rand
+  end
 end
 
 # /echo [message]: [message]を返します
 bot.command :echo do |event|
-    s = event.content
-    s.slice!(prefix + 'echo').strip!
-    unless s.empty?
-        event << s
-    else
-        event << "**error** argument is missing.\nexpected `/echo [message]`\nex. `/echo hello` > hello"
-    end
+  s = event.content
+  s.slice!(prefix + 'echo').strip!
+  unless s.empty?
+    event << s
+  else
+    event << "**error** argument is missing.\nexpected `/echo [message]`\nex. `/echo hello` > hello"
+  end
 end
 
 # /say [message]: [message]を返します
 bot.command :say do |event|
-    s = event.content
-    s.slice!(prefix + 'say').strip!
-    unless s.empty?
-        event << s
-    else
-        event << "**error** argument is missing.\nexpected `/say [message]`\nex. `/say hello` > hello"
-    end
+  s = event.content
+  s.slice!(prefix + 'say').strip!
+  unless s.empty?
+    event << s
+  else
+    event << "**error** argument is missing.\nexpected `/say [message]`\nex. `/say hello` > hello"
+  end
 end
 
 bot.command :bold do |event, *args|
-    # Again, the return value of the block is sent to the channel
-    "**#{args.join(' ')}**"
+  # Again, the return value of the block is sent to the channel
+  "**#{args.join(' ')}**"
 end
   
 bot.command :italic do |event, *args|
-    "*#{args.join(' ')}*"
+  "*#{args.join(' ')}*"
 end
 
 # /iam [role]: 役職[role]になります
 bot.command [:iam, :im] do |event, *args|
-    role_name = args.join(' ')
-    if event.server.roles.find{|r| r.name == role_name}.nil?
-        event << "「" + role_name + "」という役職はこのサーバーには存在しません"
-        return
-    elsif !event.user.roles.find{|r| r.name == role_name}.nil?
-        event << "あなたは既に「" + role_name + "」です"
-        return
-    elsif year_roles.include? role_name
-        event << "あなたは「" + role_name + "」という年代になることはできません"
-        event << "もし、割り当てられた年代が違う場合は、管理者に連絡してください"
-        return
-    elsif major_roles.include? role_name
-        event << "あなたは「" + role_name + "」という学科になることはできません"
-        event << "もし、割り当てられた学科が違う場合は、管理者に連絡してください"
-        return
-    else
-        event.user.add_role(event.server.roles.find{|r| r.name == role_name})
-        event << "あなたは「" + role_name + "」になりました"
-    end
+  role_name = args.join(' ')
+  if event.server.roles.find{|r| r.name == role_name}.nil?
+    event << "「" + role_name + "」という役職はこのサーバーには存在しません"
+    return
+  elsif !event.user.roles.find{|r| r.name == role_name}.nil?
+    event << "あなたは既に「" + role_name + "」です"
+    return
+  elsif year_roles.include? role_name
+    event << "あなたは「" + role_name + "」という年代になることはできません"
+    event << "もし、割り当てられた年代が違う場合は、管理者に連絡してください"
+    return
+  elsif major_roles.include? role_name
+    event << "あなたは「" + role_name + "」という学科になることはできません"
+    event << "もし、割り当てられた学科が違う場合は、管理者に連絡してください"
+    return
+  else
+    event.user.add_role(event.server.roles.find{|r| r.name == role_name})
+    event << "あなたは「" + role_name + "」になりました"
+  end
 end
 
 # /iamnot [role]: 役職[role]をやめます
 bot.command [:iamnot, :imnot] do |event, *args|
-    role_name = args.join(' ')
-    if event.server.roles.find{|r| r.name == role_name}.nil?
-        event << "「" + role_name + "」という役職はこのサーバーには存在しません"
-        return
-    elsif event.user.roles.find{|r| r.name == role_name}.nil?
-        event << "あなたは既に「" + role_name + "」ではありません"
-        return
-    elsif year_roles.include? role_name
-        event << "あなたは「" + role_name + "」という年代をやめることはできません"
-        event << "もし、割り当てられた年代が違う場合は、管理者に連絡してください"
-        return
-    elsif major_roles.include? role_name
-        event << "あなたは「" + role_name + "」という学科をやめることはできません"
-        event << "もし、割り当てられた学科が違う場合は、管理者に連絡してください"
-        return
-    else
-        event.user.remove_role(event.server.roles.find{|r| r.name == role_name})
-        event << "あなたは「" + role_name + "」をやめました"
-    end
+  role_name = args.join(' ')
+  if event.server.roles.find{|r| r.name == role_name}.nil?
+    event << "「" + role_name + "」という役職はこのサーバーには存在しません"
+    return
+  elsif event.user.roles.find{|r| r.name == role_name}.nil?
+    event << "あなたは既に「" + role_name + "」ではありません"
+    return
+  elsif year_roles.include? role_name
+    event << "あなたは「" + role_name + "」という年代をやめることはできません"
+    event << "もし、割り当てられた年代が違う場合は、管理者に連絡してください"
+    return
+  elsif major_roles.include? role_name
+    event << "あなたは「" + role_name + "」という学科をやめることはできません"
+    event << "もし、割り当てられた学科が違う場合は、管理者に連絡してください"
+    return
+  else
+    event.user.remove_role(event.server.roles.find{|r| r.name == role_name})
+    event << "あなたは「" + role_name + "」をやめました"
+  end
 end
 
 
 thread1 = Thread.new do
-    timers = Timers::Group.new
-    timer = timers.every(60 * 15) { bot.game = games[rand(games.count)] }
-    loop { timers.wait }
+  timers = Timers::Group.new
+  timer = timers.every(60 * 15) { bot.game = games[rand(games.count)] }
+  loop { timers.wait }
 end
 thread2 = Thread.new do
-    bot.run
+  bot.run
 end
 
 thread1.join
